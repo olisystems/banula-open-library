8eb8fcdb5b47477fb27a840b6564bc6606ea2230
Matheus Rosendo <matheus.rosendo@my-oli.com>
2026-01-30 15:02:02 -0300

    feat: remove bean dependencies from OcnClient class

--- src/main/java/com/banula/openlib/ocpi/annotation/processor/AuthorizeTokenB.java
+++ src/main/java/com/banula/openlib/ocpi/annotation/processor/AuthorizeTokenB.java
@@ -1,12 +1,5 @@
 package com.banula.openlib.ocpi.annotation.processor;
 
-import com.banula.openlib.ocn.client.OcnClient;
-import com.banula.openlib.ocpi.util.AuthenticationUtils;
-import com.banula.openlib.ocpi.exception.OCPICustomException;
-import com.banula.openlib.ocpi.util.Constants;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -14,13 +7,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.banula.openlib.ocn.client.OcnClient;
+import com.banula.openlib.ocpi.exception.OCPICustomException;
+import com.banula.openlib.ocpi.util.AuthenticationUtils;
+import com.banula.openlib.ocpi.util.Constants;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Aspect
 @Component
-@AllArgsConstructor
 public class AuthorizeTokenB {
-
-    private final OcnClient ocnClient;
 
     @Around("@annotation(com.banula.openlib.ocpi.annotation.AuthorizeTokenB)")
     public Object processAuthorizationHeader(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -33,7 +31,7 @@ public class AuthorizeTokenB {
 
     private void authorizeRequestToken(String tokenB) {
         if (tokenB == null
-                || !AuthenticationUtils.extractToken(tokenB).equals(ocnClient.getConfiguration().getTokenB())) {
+                || !AuthenticationUtils.extractToken(tokenB).equals(OcnClient.configuration.getTokenB())) {
             log.error("Invalid Token B: " + tokenB);
             throw new OCPICustomException("Invalid Token B.", Constants.STATUS_CODE_AUTHORIZATION_EXCEPTION_CODE);
         }
